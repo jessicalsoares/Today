b2e8ebec37dd6d7c6bc3e579f6dd36d11a70862a
Jessica Soares <jess.soares@hotmail.com>
2023-10-17 17:41:46 +0100

    correçoes

--- Today/ListViewController/ReminderListViewController.swift
+++ Today/ListViewController/ReminderListViewController.swift
@@ -3,6 +3,16 @@ import UIKit
 class ReminderListViewController: UICollectionViewController {
     var dataSource: DataSource!
     var reminders: [Reminder] = Reminder.sampleData
+    var listStyle: ReminderListStyle = .today
+    var filteredReminders: [Reminder] {
+        return reminders.filter { listStyle.shouldInclude(date: $0.dueDate) }.sorted {
+                    $0.dueDate < $1.dueDate
+                }
+        }
+    
+    let listStyleSegmentedControl = UISegmentedControl(items: [
+          ReminderListStyle.today.name, ReminderListStyle.future.name, ReminderListStyle.all.name
+      ])
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +33,12 @@ class ReminderListViewController: UICollectionViewController {
         addButton.accessibilityLabel = NSLocalizedString(
             "Add reminder", comment: "Add button accessibility label")
         navigationItem.rightBarButtonItem = addButton
+        
+        listStyleSegmentedControl.selectedSegmentIndex = listStyle.rawValue
+        listStyleSegmentedControl.addTarget(
+            self, action: #selector(didChangeListStyle(_:)), for: .valueChanged)
+        navigationItem.titleView = listStyleSegmentedControl
+        
         if #available(iOS 16, *) {
                     navigationItem.style = .navigator
                 }
@@ -35,7 +51,7 @@ class ReminderListViewController: UICollectionViewController {
     override func collectionView(
         _ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath
     ) -> Bool {
-        let id = reminders[indexPath.item].id
+        let id = filteredReminders[indexPath.item].id
         pushDetailViewForReminder(withId: id)
         return false
     }
